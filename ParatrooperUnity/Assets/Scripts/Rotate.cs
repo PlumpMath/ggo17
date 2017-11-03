4c70d459c904ea7f47f6da34885cbf9c4cd0153f
Matt Van Der Westhuizen <matt@chaotik.co.za>
2017-11-03 09:29:30 +0200

    Basic gun rotation and shooting mechanics are implemented!

---
+++ ParatrooperUnity/Assets/Scripts/Rotate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+	public Transform GunJoint;
+	public float DegreesPerSecond = 60.0f;
+
+	private float _rotation = 0.0f;
+	private float _change = 0.0f;
+	
+	void Update ()
+	{
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			_change = DegreesPerSecond * Time.deltaTime;
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			_change = -DegreesPerSecond * Time.deltaTime;
+		}
+		else
+		{
+			_change = 0.0f;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (Math.Abs(_change) < 0.01f) return;
+
+		var endZ = _rotation + _change;
+		if (endZ >= 90.0f)
+		{
+			_change -= endZ - 90.0f;
+		}
+		else if (endZ <= -90.0f)
+		{
+			_change -= endZ + 90.0f;
+		}
+
+		_rotation += _change;
+		GunJoint.transform.rotation = Quaternion.Euler(0, 0, _rotation);
+	}
+}
--- ParatrooperUnity/Assets/Scripts/Shoot.cs
+++ ParatrooperUnity/Assets/Scripts/Shoot.cs
@@ -1,21 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
 
 	public Transform BulletPrefab;
+	public Transform GunJoint;
+	public Transform BulletSpawn;
+	public float ShotVelocity = 1000.0f;
+	public float RechargeSeconds = 0.5f;
 
 	private float _coolDown = 0.0f;
-	private Transform _turret;
-	private Transform _bulletSpawn;
+	
 	
 	void Start ()
 	{
-		_turret = transform.Find("Turret");
-		_bulletSpawn = _turret.Find("Gun").Find("BulletSpawn");
-
 	}
 	
 	void Update ()
@@ -24,7 +22,7 @@ public class Shoot : MonoBehaviour
 
 		if (_coolDown > 0) return;
 		
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetKey(KeyCode.UpArrow))
 		{
 			Fire();
 		}
@@ -32,11 +30,11 @@ public class Shoot : MonoBehaviour
 
 	private void Fire()
 	{
-		_coolDown = 1.0f;
+		_coolDown = RechargeSeconds;
 
-		var direction = _bulletSpawn.position - _turret.position;
-		var spawn = _bulletSpawn.position;
-		var bullet = Instantiate(BulletPrefab, spawn, Quaternion.identity);
-		bullet.GetComponent<Rigidbody2D>().AddForce((spawn - direction) * 1500);
+		var direction = (BulletSpawn.position - GunJoint.position).normalized;
+		var spawnPos = BulletSpawn.position;
+		var bullet = Instantiate(BulletPrefab, spawnPos, Quaternion.identity);
+		bullet.GetComponent<Rigidbody2D>().AddForce(direction * ShotVelocity);
 	}
 }
