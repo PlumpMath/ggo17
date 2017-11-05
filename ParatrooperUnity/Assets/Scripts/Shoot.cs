67bbb365c6faec99408c68a365e0cee5b5a5d9d3
Sas van der Westhuizen <sasvdw@gmail.com>
2017-11-05 11:59:36 +0200

    Added PEW! sound to provide audio indication of shots fired.
    Have a look at Shoot.cs. I've seen the use of [SerializeField] to be one that generally provides better encapsulation so dependencies for a component doesn't have to be exposed through public fields

--- ParatrooperUnity/Assets/Scripts/Shoot.cs
+++ ParatrooperUnity/Assets/Scripts/Shoot.cs
@@ -9,11 +9,17 @@ public class Shoot : MonoBehaviour
 	public float ShotVelocity = 1000.0f;
 	public float RechargeSeconds = 0.5f;
 
+	[SerializeField]
+	private AudioClip pew;
+
 	private float _coolDown = 0.0f;
+
+	private AudioSource bulletSpawnAudioSource;
 	
 	
 	void Start ()
 	{
+		bulletSpawnAudioSource = BulletSpawn.GetComponent<AudioSource>();
 	}
 	
 	void Update ()
@@ -36,5 +42,7 @@ public class Shoot : MonoBehaviour
 		var spawnPos = BulletSpawn.position;
 		var bullet = Instantiate(BulletPrefab, spawnPos, Quaternion.identity);
 		bullet.GetComponent<Rigidbody2D>().AddForce(direction * ShotVelocity);
+
+		bulletSpawnAudioSource.PlayOneShot(this.pew);
 	}
 }
