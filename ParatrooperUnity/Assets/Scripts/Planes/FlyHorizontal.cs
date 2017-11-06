d54681b4ee71cb788aea24ba10722a9668888786
Matt Van Der Westhuizen <matt@chaotik.co.za>
2017-11-06 23:45:31 +0200

    Basic troop transport plane that drops paratroopers.

---
+++ ParatrooperUnity/Assets/Scripts/Planes/FlyHorizontal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class FlyHorizontal : MonoBehaviour
+{
+
+	[SerializeField]
+	private float Speed;
+	
+	void Start () {
+		var body =  GetComponent<Rigidbody2D>();
+		body.velocity = new Vector3(-Speed, 0, 0);
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ ParatrooperUnity/Assets/Scripts/Planes/Paradrop.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Paradrop : MonoBehaviour
+{
+
+	public Transform Paratrooper;
+	public float MinDropDelay = 1.0f;
+	public float MaxDropDelay = 2.0f;
+	public int MaxDrops = 2;
+	
+	private Transform spawn;
+	private int dropped = 0;
+	private float timer;
+	
+	void Start ()
+	{
+		spawn = transform.Find("Spawn");
+
+		SetJumpTimer();
+	}
+
+	void Update ()
+	{
+		if (dropped == MaxDrops) return;
+		
+		timer -= Time.deltaTime;
+
+		if (timer <= 0)
+		{
+			Drop();
+		}
+	}
+	
+	private void Drop()
+	{
+		Instantiate(Paratrooper, spawn.position, Quaternion.identity);
+		
+		dropped++;
+		
+		SetJumpTimer();
+	}
+	
+	private void SetJumpTimer()
+	{
+		timer = MinDropDelay + (MaxDropDelay - MinDropDelay) * Random.value;
+	}
+}
