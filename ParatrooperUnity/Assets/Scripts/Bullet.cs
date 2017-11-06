fc0bb6d133aa29a0733d1704ea849adcb82a66d9
Sas van der Westhuizen <sasvdw@gmail.com>
2017-11-06 23:55:21 +0200

    Refactored things a bit more into components that can be slapped onto game objects as stand alone features.

---
+++ ParatrooperUnity/Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bullet : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody2D;
+
+    private void Awake()
+    {
+        this.rigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    public void AddForce(Vector2 force)
+    {
+        this.rigidbody2D.AddForce(force);
+    }
+}
--- ParatrooperUnity/Assets/Scripts/Shoot.cs
+++ ParatrooperUnity/Assets/Scripts/Shoot.cs
@@ -2,47 +2,16 @@
 
 public class Shoot : MonoBehaviour
 {
-
-	public Transform BulletPrefab;
-	public Transform GunJoint;
-	public Transform BulletSpawn;
-	public float ShotVelocity = 1000.0f;
-	public float RechargeSeconds = 0.5f;
-
 	[SerializeField]
-	private AudioClip pew;
+	private Transform gunJoint;
+	[SerializeField]
+	private Gun gun;
 
-	private float _coolDown = 0.0f;
-
-	private AudioSource bulletSpawnAudioSource;
-	
-	
-	void Start ()
-	{
-		bulletSpawnAudioSource = BulletSpawn.GetComponent<AudioSource>();
-	}
-	
 	void Update ()
 	{
-		_coolDown -= Time.deltaTime;
-
-		if (_coolDown > 0) return;
-		
 		if (Input.GetKey(KeyCode.UpArrow))
 		{
-			Fire();
+			this.gun.Fire(this.gunJoint);
 		}
-	}
-
-	private void Fire()
-	{
-		_coolDown = RechargeSeconds;
-
-		var direction = (BulletSpawn.position - GunJoint.position).normalized;
-		var spawnPos = BulletSpawn.position;
-		var bullet = Instantiate(BulletPrefab, spawnPos, Quaternion.identity);
-		bullet.GetComponent<Rigidbody2D>().AddForce(direction * ShotVelocity);
-
-		bulletSpawnAudioSource.PlayOneShot(this.pew);
 	}
 }
