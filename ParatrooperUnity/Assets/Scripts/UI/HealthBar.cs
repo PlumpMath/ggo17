a1da44a8626b9f0162377a4feeab7d9b40311102
Matt Van Der Westhuizen <matt@chaotik.co.za>
2017-11-18 12:54:43 +0200

    Added UI for representing the health of the bunker crew.

---
+++ ParatrooperUnity/Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+
+	[SerializeField]
+	private float health = 10.0f;
+	[SerializeField]
+	private float maxHealth = 10.0f;
+	
+	private RectTransform bar;
+	private float maxWidth = 0.0f;
+	
+	void Awake()
+	{
+		var barTransform = transform.Find("Bar");
+		if (barTransform != null)
+		{
+			bar = barTransform.GetComponent<RectTransform>();
+		}
+
+		if (bar != null)
+		{
+			maxWidth = bar.rect.width;
+		}
+	}
+	
+	void Update ()
+	{
+		bar.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, maxWidth / maxHealth * health);
+	}
+}
