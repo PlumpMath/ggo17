0b87695b16720904c547ea0aed7c108ba6d62098
Sas van der Westhuizen <sasvdw@gmail.com>
2017-11-19 16:52:46 +0200

    Fixing up some namespacing issues

--- ParatrooperUnity/Assets/Scripts/Lanes/LaneManager.cs
+++ ParatrooperUnity/Assets/Scripts/Lanes/LaneManager.cs
@@ -1,92 +1,93 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-
-public class LaneManager : MonoBehaviour
+namespace Lanes
 {
+    public class LaneManager : MonoBehaviour
+    {
+        [SerializeField]
+        private FlyHorizontal[] planePrefabs;
 
-	[SerializeField]
-	private FlyHorizontal planePrefab;
-	[SerializeField]
-	private FlyHorizontal[] planePrefabs;
-	[SerializeField]
-	private float spawnDelay = 2.0f;
-	[SerializeField]
-	private bool autoSpawn = true;
+        [SerializeField]
+        private float spawnDelay = 2.0f;
 
-	private float delay;
-	private Transform spawnLeft;
-	private Transform spawnRight;
-	private List<Transform> planes;
+        [SerializeField]
+        private bool autoSpawn = true;
 
-	void Awake()
-	{
-		delay = spawnDelay;
-		spawnLeft = transform.Find("SpawnLeft");
-		spawnRight = transform.Find("SpawnRight");
-		planes = new List<Transform>(2);
-	}
-	
-	void Update()
-	{
-		if (!autoSpawn) return;
-		
-		delay -= Time.deltaTime;
-		
-		if (planes.Count == 0 && delay < 0)
-		{
-			if (Random.value < 0.5f)
-			{
-				SpawnLeft();
-			}
-			else
-			{
-				SpawnRight();
-			}
-		}
-	}
+        private float delay;
+        private Transform spawnLeft;
+        private Transform spawnRight;
+        private List<Transform> planes;
 
-	public void SpawnLeft()
-	{
-		var plane = Spawn(spawnLeft);
-		plane.FlyRight();
-	}
+        public void SpawnLeft()
+        {
+            var plane = this.Spawn(this.spawnLeft);
+            plane.FlyRight();
+        }
 
-	public void SpawnRight()
-	{
-		var plane = Spawn(spawnRight);
-		plane.FlyLeft();
-	}
+        public void SpawnRight()
+        {
+            var plane = this.Spawn(this.spawnRight);
+            plane.FlyLeft();
+        }
 
-	private FlyHorizontal Spawn(Transform spawn)
-	{
-		delay = spawnDelay;
+        void Awake()
+        {
+            this.delay = this.spawnDelay;
+            this.spawnLeft = this.transform.Find("SpawnLeft");
+            this.spawnRight = this.transform.Find("SpawnRight");
+            this.planes = new List<Transform>(2);
+        }
 
-		var planeToSpawn = this.planePrefabs[Random.Range(0, this.planePrefabs.Length)];
+        void Update()
+        {
+            if(!this.autoSpawn) return;
 
-		return PoolingFactory.SpawnOrRecycle<FlyHorizontal>(planeToSpawn.transform, spawn.position);
-	}
+            this.delay -= Time.deltaTime;
 
-	private void OnTriggerEnter2D(Collider2D other)
-	{
-		var plane = other.GetComponent<FlyHorizontal>();
-		if (plane != null)
-		{
-			planes.Add(other.transform);
-		}
-	}
+            if(this.planes.Count == 0 && this.delay < 0)
+            {
+                if(Random.value < 0.5f)
+                {
+                    this.SpawnLeft();
+                }
+                else
+                {
+                    this.SpawnRight();
+                }
+            }
+        }
 
-	private void OnTriggerExit2D(Collider2D other)
-	{
-		var plane = other.GetComponent<FlyHorizontal>();
-		if (plane != null)
-		{
-			planes.Remove(other.transform);
-			var pooled = other.transform.GetComponent<Pooled>();
-			if (pooled != null)
-			{
-				pooled.DestroyPooled();
-			}
-		}
-	}
+        private FlyHorizontal Spawn(Transform spawn)
+        {
+            this.delay = this.spawnDelay;
+
+            var planeToSpawn = this.planePrefabs[Random.Range(0, this.planePrefabs.Length)];
+
+            return PoolingFactory.SpawnOrRecycle<FlyHorizontal>(planeToSpawn.transform, spawn.position);
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var plane = other.GetComponent<FlyHorizontal>();
+            if(plane != null)
+            {
+                this.planes.Add(other.transform);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            var plane = other.GetComponent<FlyHorizontal>();
+            if(plane != null)
+            {
+                this.planes.Remove(other.transform);
+                var pooled = other.transform.GetComponent<Pooled>();
+                if(pooled != null)
+                {
+                    pooled.DestroyPooled();
+                }
+            }
+        }
+    }
 }
